9d8c9c08e7963dad317ec6c76673c1f2b52d7b7c
clj55 <103747880+clj55@users.noreply.github.com>
2023-05-01 21:50:45 +0800

    FundRecovery & BankPerformance
    Pushing FundRecovery graph and second dashboard from Yudong and Bank performance table

--- utils/d2_bankperformance.py
+++ utils/d2_bankperformance.py
@@ -4,34 +4,75 @@ from nicegui import ui
 
 connection = pymysql.connect(host = '119.74.24.181', user = 'htx', password = 'Police123456', database = 'ASTRO')
 
-df = pd.read_sql_query("""SELECT report_number, account_bank, datetime_production_order_served, datetime_bank_account_frozen, amount_scammed FROM scam_management_system""", connection)
+### Data Processing 
 
-df['reaction_time'] =  df['datetime_bank_account_frozen']- df['datetime_production_order_served']
+df = pd.read_sql_query("""SELECT account_bank, datetime_production_order_served, datetime_bank_account_frozen, amount_scammed
+FROM scam_management_system order by account_bank asc""", connection)
+
+#   Data cleaning 
+df['account_bank'] = df['account_bank'].str.upper()
+
+#   Calculations
 groupedBank = df.groupby('account_bank')
-
 agg_function = {'amount_scammed':['min', 'max', 'sum', 'mean'],
-                'reaction_time': ['min', 'max', 'sum', 'mean'],
                 'datetime_production_order_served': 'count',
                 }
-
 stats = groupedBank.agg(agg_function)
 stats.columns = ['-'.join(col) for col in stats.columns.values]
-banks = stats.index.to_list()
+stats = stats.reset_index()
 
+#   Preparing Data to display
+to_display_df = stats[['account_bank', 'datetime_production_order_served-count','amount_scammed-mean']].round(2)
+to_display_df = to_display_df.rename(columns = {'Account_bank': "Bank", 
+                                'datetime_production_order_served-count': 'Number of Production Orders Sent',
+                                'amount_scammed-mean':'Amount Scammed',
+                                })
 
-#implement a sort later
+###     Grid
+grid = ui.aggrid.from_pandas(to_display_df).classes('max-h-70')
+grid.options['columnDefs'][0].update({'filter':'agTextColumnFilter'})
+grid.options['columnDefs'][1].update({'filter':'agNumberColumnFilter',
+                                      'filterParams':{
+                                          'filterOptions':['lessThan', 
+                                                           'lessThanOrEqual', 
+                                                           'greaterThan', 
+                                                           'greaterThanOrEqual',
+                                                           'inRange']}
+                                      }
+                                      )
+grid.options['columnDefs'][2].update({'filter':'agNumberColumnFilter',
+                                      'filterParams':{
+                                          'filterOptions':['lessThan', 
+                                                           'lessThanOrEqual', 
+                                                           'greaterThan', 
+                                                           'greaterThanOrEqual',
+                                                           'inRange']}
+                                          })
+grid.options.update(
+        {'defaultColDef':{
+            'sortable':True, 
+            'floatingFilter': True,
+            'suppressMenu' : True,
+        },
+        })
 
-dropdown = ui.select(['avg', 'min', 'sum', 'mean'], value='mean') #later rename sum to Total
+# for column in grid.options['columnDefs']:
+#     column['sortable']  = True
+#     column['floatingFilter']  = True
+print(grid.options)
+
+######################3
+ui.aggrid
+
+dropdown = ui.select(['min', 'max', 'sum', 'mean'], value='mean', on_change = lambda x:update(x.value))
+
+def update(x):
+    for i in range(len(stats.index)):
+        grid.options['rowData'][i]['Amount Scammed'] = stats['amount_scammed-'+x][i]
+    grid.update()
+
 
 
 #   Bank's Performance Table
-
-grid = ui.aggrid.from_pandas(stats).classes('max-h-40')
-grid
-
-def update():
-    grid.options['rowData'][0]['age'] += 1
-    grid.update()
-
 
 ui.run()
---
+++ utils/d2_fundrecovery.py
@@ -0,0 +1,86 @@
+from nicegui import ui
+
+def fund_recovery_plot(df):
+    df_amount_recovery = df[['amount_scammed', 'latest_balance_seized']].copy()
+    df_amount_recovery = df_amount_recovery.fillna(0)
+
+    amount_scammed = 0
+    for i in range(len(df_amount_recovery.index)):
+        amount_scammed += df_amount_recovery['amount_scammed'][i]
+        
+    amount_recover= 0
+    for i in range(len(df_amount_recovery.index)):
+        if df_amount_recovery['latest_balance_seized'][i] >= df_amount_recovery['amount_scammed'][i]:
+            amount_recover += df_amount_recovery['amount_scammed'][i]
+        
+        else:
+            amount_recover += df_amount_recovery['latest_balance_seized'][i]
+
+    amount_recover = round(amount_recover, 2)
+    amount_scammed = round(amount_scammed, 2)
+
+    amount_recover_percentage = amount_recover / amount_scammed * 100
+    amount_recover_percentage = round(amount_recover_percentage, 1)
+    amount_scammed_percentage = 100.0 - amount_recover_percentage
+
+    chart = ui.chart({
+                'chart': {
+                    'type': 'pie',
+                    'backgroundColor': 'rgba(0,0,0,0)',
+                },
+                
+                'title': {
+                    'text': f'Scam Amount <br> ${amount_scammed:,} <br><br><br> Recovered Amount <br> ${amount_recover:,}',
+                    'align': 'center',
+                    'verticalAlign': 'middle',
+                    'style': {
+                        'color': '#CED5DF'
+                    }
+                },
+                
+                'credits': {'enabled': False},
+
+                'accessibility': {
+                    'announceNewData': {'enabled': True},
+                    'point': {'valueSuffix': '%'}
+                },
+
+                'plotOptions': {
+                    'series': {
+                        'dataLabels': {
+                            'enabled': True,
+                            'format': '{point.name}: {point.y:.1f}%'
+                        }
+                    },
+                    'pie': {
+                        'borderColor': None,
+                        'borderWidth': 8,
+                        'innerSize': '98%',
+                    }
+                },
+
+                'tooltip': {
+                    'headerFormat': '<span style="font-size:11px">{series.name}</span><br>',
+                    'pointFormat': '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+                },
+
+                'series': [
+                    {
+                        'colorByPoint': True,
+                        'data': [
+                            {
+                                'name': 'Recovered',
+                                'y': amount_recover_percentage,
+                                'color': 'rgba(92, 255, 127, 0.7)',
+                            },
+                            {
+                                'name': 'Loss',
+                                'y': amount_scammed_percentage,
+                                'color': 'rgba(255, 44, 104, 0.7)',
+                            }
+                        ]
+                    }
+                ],
+            })
+
+    return chart
